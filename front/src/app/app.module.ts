0468a01605bc3552d8a400034ad85c170c01c797
Ryan Fleharty <ryan.fleharty@gmail.com>
2018-03-14 21:27:59 -0500

    Added angular routing

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,16 +4,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { MapService } from './map.service';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './/app-routing.module';
+import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    DashboardComponent
   ],
   imports: [
     FormsModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [MapService],
   bootstrap: [AppComponent]
